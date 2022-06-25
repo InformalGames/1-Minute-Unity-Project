9bec2c52c58038236d61db4f2177a215c02c9265
Unknown Chan <anothergamedevstudio@gmail.com>
2022-06-25 14:15:08 -0400

    Added a in-game Timer and UI
    Go to the Camera gameobject for the player, The Timer script is put there. If you set the bool on there to true for the timer, the timer will count towards 1 minute.
    
    The UI is also made to reflect that.

--- 1Minute/Assets/Scripts/TimerControl.cs
+++ 1Minute/Assets/Scripts/TimerControl.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class TimerControl : MonoBehaviour
 {
 
@@ -15,9 +15,16 @@ public class TimerControl : MonoBehaviour
 
     public float RateOfTime; //how fast the timer goes. 
 
-    public float CurrentRateOfTime; //
+    public float CurrentRateOfTime; //this variable is a timer. Once "CurrentRateOfTime" is larger then the number of RateOfTime, CurrentTime int will add a number
 
-    
+    //UI components
+    public GameObject TimerDisplayOBJ;
+
+    string TimerUI;
+
+    //setting
+
+    bool IsPaused;
 
     // Start is called before the first frame update
     void Awake()
@@ -25,13 +32,59 @@ public class TimerControl : MonoBehaviour
         DeathTimer = 60;
         CurrentTime = 0;
         TimerActivated = false;
-
-        RateOfTime = 3;
+        CurrentRateOfTime = 0;
+        RateOfTime = 1.2f;
+        TimerDisplayOBJ = transform.Find("Canvas").transform.Find("TimerTxt").gameObject;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        IsPaused = GameObject.Find("GameController").GetComponent<GameSetting>().PauseGame;
+        //Display UI
+        if (CurrentTime < 60)
+        {
+            if (CurrentTime < 10)
+            {
+                TimerUI = "0:0" + CurrentTime;
+            }
+            else
+            {
+                TimerUI = "0:" + CurrentTime;
+            }
+            
+        }
+        else
+        {
+            TimerUI = "1:00!!!";
+        }
+        TimerDisplayOBJ.GetComponent<TMPro.TextMeshProUGUI>().text = "Timer: " + TimerUI;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (IsPaused == false)
+        {
+            //check if the timer is working right now
+            if (TimerActivated == true)
+            {
+                TimerFunction();
+            }
+        }
         
+    }
+    //controls the timer itself, makes the timer tick.
+    public void TimerFunction()
+    {
+        if (CurrentRateOfTime < RateOfTime) 
+        {
+            CurrentRateOfTime += 1 * Time.deltaTime; //increase the current rate of time
+        }
+        else
+        {
+            if (CurrentRateOfTime > RateOfTime)
+            {
+                CurrentTime += 1;
+                CurrentRateOfTime = 0;
+            }
+        }
     }
 }
